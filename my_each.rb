d2eef9f4800a75afa12f9d58ad9f086559634b23
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-20 03:40:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,5 @@
 def my_each(array)
   i = 0
-  while i < array.length
-# The "while" method will always return a value of => nil"
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-def my_each(array)
-  i = 0
   collection = []
   while i < array.length
     collection << yield(array[i])
