b4ce673dfd034d0e256d512035a273d6ed282e21
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-20 03:40:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,3 +6,13 @@ def my_each(array)
     i = i + 1
   end
 end
+
+def my_each(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
